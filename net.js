9d801b436d45b7ae15c921bb6b38666617a943f1
Peter <Peter1992t@gmail.com>
2015-04-30 13:56:25 -0500

    improved functionality

--- net.js
+++ net.js
@@ -1,55 +1,78 @@
 var Twit = require('twit');
 var fs = require('fs');
 
+var T = new Twit
+({
+	consumer_key:           'XzjjKWXFe9OxmOmyCpQklxNmR'
+	, consumer_secret:      '2yIsXYliWhxxOjimIdrX01aaw8wNMvhpedRR9ehFZoCMYoJCp0'
+	, access_token:         '3171126109-pyE2vLmIRiB5wLhzptiZKS3j6fgqhZxMhi600Mx'
+	, access_token_secret:  'jInajNq27AUhj3cw3lXlema3PqgquXN7nhxT3G4W6hpER'
+})
+
+cleanDir('./unparsedTweets');
+cleanDir('./parsedTweets');
+
 function cleanDir(path)
 {
-	if(fs.existsSync(path))
+	if (fs.existsSync(path))
 	{
 	    fs.readdirSync(path).forEach(function(file,index)
 	    {
 			var curPath = path + "/" + file;
-			if(fs.lstatSync(curPath).isDirectory())
-			{
+			if (fs.lstatSync(curPath).isDirectory())
 				deleteFolderRecursive(curPath);
-			} 
+
 			else
-			{
-			fs.unlinkSync(curPath);
-			}
+				fs.unlinkSync(curPath);
 		});
 	}
 }
 
-function net(stopTime, doneNetting)
+function filter(stopTime, string, doneNetting)
 {
-	var T = new Twit
-	({
-	    consumer_key:         'H8XDIUGhj1MbwGrdevTWjAE42'
-	  , consumer_secret:      'Gfwhp84TS2hpbooBJ1lYAYbFE9zvtk5BuronpYoOw5fiCuT9oQ'
-	  , access_token:         '3171126109-V5djIva6b07Ju9rf4OQiFfN0mqTI8WKmJFIER9K'
-	  , access_token_secret:  'zTylnQhRKuGiGhIibKjQU2RGjLyp3fT0JoCokdGnzyMfN'
-	})
+	var stream = T.stream('statuses/filter', { track: [string]});
 
-	cleanDir('./unparsedTweets');
-	cleanDir('./parsedTweets');
-
-	var stream = T.stream('statuses/sample')
-
-	i = 0;
+	var i = 0;
 	stream.on('tweet', gotTweet)
+	setTimeout(timesUp, stopTime);
 
 	function gotTweet(tweet)
 	{
 		fs.writeFileSync('./unparsedTweets/tweet' + i + '.txt', JSON.stringify(tweet))
-
 		i++;
-		if ((Date.now() / 1000 | 0) > stopTime)
-		{
-			stream.stop();
-			console.log('Caught ' + i + ' tweets!');
-			doneNetting(i);
-		}
+	}
+
+	function timesUp()
+	{
+		stream.stop();
+		console.log('\nCaught ' + i + ' tweets!');
+		console.log('Please check the parsedTweets directory for the tweets.')
+		doneNetting(i);
 	}
 }
 
-module.exports = net;
+function sample(stopTime, doneNetting)
+{
+	var stream = T.stream('statuses/sample');
+
+	var i = 0;
+	stream.on('tweet', gotTweet)
+	setTimeout(timesUp, stopTime);
+
+	function gotTweet(tweet)
+	{
+		fs.writeFileSync('./unparsedTweets/tweet' + i + '.txt', JSON.stringify(tweet))
+		i++;
+	}
+
+	function timesUp()
+	{
+		stream.stop();
+		console.log('\nCaught ' + i + ' tweets!');
+		console.log('Please check the parsedTweets directory for the tweets.')
+		doneNetting(i);
+	}
+}
+
+exports.filter = filter;
+exports.sample = sample;
--- twitterBot.js
+++ twitterBot.js
@@ -1,8 +1,46 @@
 var net = require('./net.js');
 var parser = require('./parser.js');
+var TwitterBot = require('prompt');
 
-var time = 5;
-var currentTime = (Date.now() / 1000 | 0);
-var stopTime = currentTime + time;
+process.stdout.write('\u001B[2J\u001B[0;0f');
+TwitterBot.start();
+console.log('Please select whether you want to make a filter request, or a sample request.');
+TwitterBot.get(['request'], requestRecieved);
 
-net(stopTime, parser);
+function requestRecieved(err, result)
+{
+	if (result.request == 'filter')
+		filterRequest();
+	else if (result.request == 'sample')
+		sampleRequest();
+	else
+		console.log('Invalid request, check your spelling.');
+}
+
+function filterRequest()
+{
+	process.stdout.write('\u001B[2J\u001B[0;0f');
+	TwitterBot.start();
+	console.log('Please specify what string you are looking for, and for how many seconds would you like to capture tweets.');
+	TwitterBot.get(['string', 'time'], filterRequestRecieved);
+
+	function filterRequestRecieved(err, filterResult)
+	{
+		time = filterResult.time * 1000;
+		net.filter(time, filterResult.string, parser);
+	}
+}
+
+function sampleRequest()
+{
+	process.stdout.write('\u001B[2J\u001B[0;0f');
+	TwitterBot.start();
+	console.log('Please specify for how many seconds would you like to capture tweets.');
+	TwitterBot.get(['time'], sampleRequestRecieved);
+
+	function sampleRequestRecieved(err, sampleResult)
+	{
+		time = sampleResult.time * 1000;
+		net.sample(time, parser);
+	}
+}
